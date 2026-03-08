4d098709857017ad6015c0fa6d7692ec8f034b4b
Sidney Luiz Carneiro <sidneylcarneiro@yahoo.com>
2026-03-07 22:55:06 -0300

    Change directory path in iniciar_fila.sh
    Updated directory path for YouTube queue script.

--- iniciar_fila.sh
+++ iniciar_fila.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Iniciando a Fila Sequencial do YouTube..."
-cd /home/sidney/Dev/youtube-fila-sequencial
+cd LOCAL-PARA-SUA-PASTA/youtube-fila-sequencial
 source venv/bin/activate
 uvicorn main:app
